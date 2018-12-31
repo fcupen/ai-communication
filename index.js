1bfb611fa0f873de0f71073f19403787027d790e
Francisco Cupen <f-918@hotmail.com>
2018-12-31 14:20:46 -0400

    socket connections btw client and server

--- index.js
+++ index.js
@@ -1,11 +1,71 @@
 'use strict';
 
+const app = require('express')();
+const http = require('http').Server(app);
+const sio = require('socket.io')(http);
+const master_server = require("socket.io-client")('http://localhost:8100'); // This is a client connecting to the SERVER 2
+
+// URL ESTANDART
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+// CREAR HTTP
+ http.listen(1918, function(){
+   console.log('listening on *:3001');
+ });
+
+// SERVER 2
+
+// CONECTAR CON SERVER
+master_server.on("connect",function(){
+console.log("MASTER SERVER Connected!");
+});
+
+// LOCAL SERVER
+sio.on('connection', onConnect);
+
+// CALLBACK LOCAL SERVER
+  function onConnect(socket){
+    console.log('SERVER CLIENT CONNECTED');
+    // EMITIR MENSAJE
+  // socket.emit('message2', 'msg');
+
+// RECIBIR MENSAJES
+    socket.on('message', function(msg){
+      // EMITIR MENSAJE AL SERVIDOR
+    master_server.emit("message", msg);
+  });
+
+  // VER SI SE DESCONECTA
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+
+  // RECIBIR MENSAJES DEL SERVIDOR
+  master_server.on('chatmessage', function(msg){
+    // EMITIR MENSAJE LOCAL
+  socket.emit('chatmessage', msg);
+  });
+
+  // master_server.emit("message","Commando: ESTO");
+}
+
+setTimeout(function(){
+  retorno('msg');
+},2000);
+function retorno(msg) {
+  sio.emit('chatmessage', msg);
+  };
 /**
  * Adds commas to a number
  * @param {number} number
  * @param {string} locale
  * @return {string}
  */
-module.exports = function(number, locale) {
-    return number.toLocaleString(locale);
-};
+ function numeros(number, locale) {
+     return number.toLocaleString(locale);
+ };
+
+module.exports.numero = numeros;
+module.exports.string = 'numero';
---
+++ server.js
@@ -0,0 +1,51 @@
+// 'use strict';
+//
+// /**
+//  * Adds commas to a number
+//  * @param {number} number
+//  * @param {string} locale
+//  * @return {string}
+//  */
+//  function numeros(number, locale) {
+//      return number.toLocaleString(locale);
+//  };
+//
+//
+// const app = require('express')();
+// const http = require('http').Server(app);
+// const sio = require('socket.io')(http);
+//
+// http.listen(3001, function(){
+//   console.log('listening on *:3001');
+//   console.log(sio);
+// });
+// app.get('/', function(req, res){
+//   res.sendFile(__dirname + '/index.html');
+// });
+//
+// sio.on('connection', function(socket){
+//   console.log('a user connected');
+//   socket.on('chat message', function(msg){
+//     sio.emit('chat message', msg);
+//   });
+//   socket.on('disconnect', function(){
+//     console.log('user disconnected');
+//   });
+// });
+
+
+// module.exports.sio = sio;
+// module.exports.string = 'numero';
+
+
+// Server 1
+var io = require("socket.io").listen(8100);
+io.sockets.on("connection",function(socket){
+    console.log("Server Connected!");
+
+    socket.on("message",function(data){
+    console.log(data);
+        socket.emit('chatmessage', data);
+        socket.broadcast.emit('chatmessage', data);
+    });
+});
